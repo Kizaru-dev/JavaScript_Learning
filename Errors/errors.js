a3cd605619fd1d19434ad53c44ec3c9f55fa1dd7
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2026-04-04 10:11:49 +0530

    learn about error and difference types of error

---
+++ Errors/errors.js
@@ -0,0 +1,16 @@
+// console.log("We are learning JavaScript");
+// console.log(age);
+// console.log("Next Line ");
+// logical error .
+
+console.log("We are learing JavaScript ");
+try{
+    // console.log(age);
+    console.log("Second Line");
+}catch(err){
+    console.log(err.name);
+}finally{
+    console.log("The program is still working ... ");
+}
+
+console.log(2 - 3);
